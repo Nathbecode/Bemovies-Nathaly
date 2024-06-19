48ef36c7373f3dcc4faabea59352953b7a3f9a11
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-19 05:02:54 +0200

    bearer token alternative added

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+// Import: API key OR bearer token.
 import { returnKey } from "./return-key.js";
-const apiKey = returnKey();
+// import { returnBearerToken } from "./return-key.js";
 
+// Variable: API key OR bearer token.
+const apiKey = returnKey();
+// const bearerToken = returnBearerToken();
+
+// Query selectors.
 const openModalBtn = document.querySelector("#openModalBtn");
 const closeBtn = document.querySelector(".close");
 const modal = document.querySelector(".modal");
 const input = document.querySelector("#input");
 const searchBtn = document.querySelector("#search-movie");
 
+// Events.
 openModalBtn.addEventListener("click", openSignin);
 closeBtn.addEventListener("click", closeSignin);
 searchBtn.addEventListener("click", search);
 input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    search();
-  }
+  if (e.key === "Enter") search();
 });
 
+// Functions.
 function openSignin() {
   modal.style.display = "block";
 }
@@ -30,9 +36,25 @@ async function search() {
   console.log(apiResponse);
 }
 
+//// First way, with API key
 async function getMoviesBySearch(movieTitle) {
   const uri = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`;
   const response = await fetch(uri);
   const json = await response.json();
   return json;
 }
+
+//// Alternative way, with Bearer token
+// async function getMoviesBySearch(movieTitle) {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       Authorization: `Bearer ${bearerToken}`,
+//     },
+//   };
+//   const uri = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1`;
+//   const response = await fetch(uri, options);
+//   const json = await response.json();
+//   return json;
+// }
