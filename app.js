99cafed9d28c8ddf07df31b63b310d62c5c3890f
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-19 04:29:31 +0200

    skeleton of fetch done

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 import { returnKey } from "./return-key.js";
+const apiKey = returnKey();
 
-// Appeler la fonction returnKey pour obtenir l'API key.
-console.log(returnKey());
+const openModalBtn = document.querySelector("#openModalBtn");
+const closeBtn = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+const input = document.querySelector("#input");
+const searchBtn = document.querySelector("#search-movie");
+
+openModalBtn.addEventListener("click", openSignin);
+closeBtn.addEventListener("click", closeSignin);
+searchBtn.addEventListener("click", search);
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    search();
+  }
+});
+
+function openSignin() {
+  modal.style.display = "block";
+}
+
+function closeSignin() {
+  modal.style.display = "none";
+}
+
+async function search() {
+  const inputValue = input.value;
+  const apiResponse = await getMoviesBySearch(inputValue);
+  console.log(apiResponse);
+}
+
+async function getMoviesBySearch(movieTitle) {
+  const uri = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`;
+  const response = await fetch(uri);
+  const json = await response.json();
+  return json;
+}
