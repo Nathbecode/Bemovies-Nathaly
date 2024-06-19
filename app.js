c7f2abacf5424b7b655ee2cef8b0c0043c1b6547
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-19 12:02:04 +0200

    now_playing replaced latest releases

--- app.js
+++ app.js
@@ -63,23 +63,8 @@ async function getMoviesBySearch(movieTitle) {
   return json;
 }
 
-// // Alternative way, with Bearer token
-// async function getMoviesBySearch(movieTitle) {
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       accept: "application/json",
-//       Authorization: `Bearer ${bearerToken}`,
-//     },
-//   };
-//   const uri = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1`;
-//   const response = await fetch(uri, options);
-//   const json = await response.json();
-//   return json;
-// }
-
 async function getLatestMovies() {
-  const uri = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=2024&region=belgium&sort_by=popularity.desc&api_key=${apiKey}`;
+  const uri = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
   const response = await fetch(uri);
   const json = await response.json();
   return json;
@@ -96,9 +81,10 @@ async function displayLatestMovies() {
     swiperSlide.classList.add("swiper-slide");
     const image = document.createElement("img");
     const poster = results[i]["poster_path"];
-    image.src =
-      `https://image.tmdb.org/t/p/w500/${poster}`;
+    image.src = `https://image.tmdb.org/t/p/w500/${poster}`;
     swiperSlide.appendChild(image);
     swiperWrapperLatest.appendChild(swiperSlide);
   }
 }
+
+//TODO : ajouter classes via DOM, pour latest swiper.
